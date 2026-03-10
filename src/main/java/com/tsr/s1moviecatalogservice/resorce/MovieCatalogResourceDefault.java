a5f662617d63d9a7164691c418709e52bbab9a2f
ravirhce <30981213+ravirhce@users.noreply.github.com>
2026-03-10 18:05:37 +0530

    Update MovieCatalogResourceDefault.java

--- src/main/java/com/tsr/s1moviecatalogservice/resorce/MovieCatalogResourceDefault.java
+++ src/main/java/com/tsr/s1moviecatalogservice/resorce/MovieCatalogResourceDefault.java
@@ -1,22 +1,14 @@
 package com.tsr.s1moviecatalogservice.resorce;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
-//import org.apache.tomcat.util.http.fileupload.RequestContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.support.RequestContext;
 
 import com.tsr.s1moviecatalogservice.model.CatalogItem;
 import com.tsr.s1moviecatalogservice.model.Movie;
-import com.tsr.s1moviecatalogservice.model.Rating;
-import com.tsr.s1moviecatalogservice.model.UserRating;
 
 @RestController
 //@RequestMapping("/")
@@ -24,15 +16,10 @@ public class MovieCatalogResourceDefault {
 	
 	static int cnt=100;
 	
-	@Autowired
-	private RestTemplate restTemplate;
-	
 	@RequestMapping("/")
 	public List<CatalogItem> getCatalog(){
-		
-		System.out.println("TSR  -- Default Calling...");
 		cnt++;
-		return restTemplate.getForObject("http://s1-git:8081/catalog/TSR.."+cnt, List.class);
+		return Arrays.asList(new CatalogItem("movie-" + cnt, "Default standalone response", 4));
 	}
 	
 	@RequestMapping("/movie/{movieId}")
