b9e96c4d071cfbf42581db0de9005a81a36af171
ravirhce <30981213+ravirhce@users.noreply.github.com>
2026-03-10 18:05:15 +0530

    Update MovieCatalogResource.java

--- src/main/java/com/tsr/s1moviecatalogservice/resorce/MovieCatalogResource.java
+++ src/main/java/com/tsr/s1moviecatalogservice/resorce/MovieCatalogResource.java
@@ -1,45 +1,25 @@
 package com.tsr.s1moviecatalogservice.resorce;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.tsr.s1moviecatalogservice.model.CatalogItem;
 import com.tsr.s1moviecatalogservice.model.Movie;
-import com.tsr.s1moviecatalogservice.model.Rating;
-import com.tsr.s1moviecatalogservice.model.UserRating;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 	
-	@Autowired
-	private RestTemplate restTemplate;
-	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable String userId){
-		
-		System.out.println("TSR11112 -- inside getCatalog()... userId="+userId);
-		System.out.println("JAVA_HOME Value:- " + System.getenv("JAVA_HOME"));
-		System.out.println("RANA_HOME Value:- " + System.getenv("RANA_HOME"));
-		
-		UserRating ratings = restTemplate.getForObject("http://s3-rating-data-service:8083/ratingsdata/users/"+userId, UserRating.class);
-		//UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"+userId, UserRating.class);
-		
-		return ratings.getUserRating().stream().map(rating -> {
-			//Movie movie = restTemplate.getForObject("http://s2-movie-info-service:8082/movies/"+rating.getMovieId(), Movie.class);
-			Movie movie = restTemplate.getForObject("http://p2-movie-info-service:5000/api/v1.0/task/id/"+rating.getMovieId(), Movie.class);
-			return new CatalogItem(movie.getName(), "Film of 2000s", rating.getRating());
-		})
-		.collect(Collectors.toList());
-				
+		return Arrays.asList(
+			new CatalogItem("Movie 1 for " + userId, "Standalone demo response", 4),
+			new CatalogItem("Movie 2 for " + userId, "Standalone demo response", 5)
+		);
 	}
 	
 	@RequestMapping("/movie/{movieId}")
